90e7576bf4a52b9b988e03a83616f055f7e537e2
isabellagouveias <Isabellagouveias@gmail.com>
2024-04-08 18:38:32 -0300

    feat: iniciando o curso (versão 17 - components)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, NewComponent],
+  styles: [],
+  template: `
+    <h1>Curso de angular</h1>
+    <new-component />
+  `,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
